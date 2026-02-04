29cf8ad7b99831ee5081f5f07d630d68fce960f0
Juan Douglas <juandouglas2004@gmail.com>
2026-02-04 17:31:47 -0300

    Corrigir erro de deploy

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  // Remove distDir to use default 'out' directory
 };
 
 export default nextConfig;
